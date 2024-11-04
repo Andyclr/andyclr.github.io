62b92493acdf2802c170924871e6501c504f9bfc
Andyclr <114229372+Andyclr@users.noreply.github.com>
2024-11-04 13:56:38 +0100

    Add Camera option and enlarge the map

--- script.js
+++ script.js
@@ -9,3 +9,22 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 
 var marker = L.marker([46.201398876908065, 6.145992279052731]).addTo(map);
+
+
+const video = document.getElementById('camera-feed');
+
+
+if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+    
+    navigator.mediaDevices.getUserMedia({ video: true })
+        .then((stream) => {
+            
+            video.srcObject = stream;
+        })
+        .catch((error) => {
+            console.error("Error accessing the camera:", error);
+            alert("Camera access is required to view the feed.");
+        });
+} else {
+    alert("Camera not supported in this browser.");
+}
