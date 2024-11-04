6c4ce4c53e05aa3038f655ba1393479502f44bfb
Andyclr <114229372+Andyclr@users.noreply.github.com>
2024-11-04 13:35:04 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 "use strict";
 
-let map = document.getElementById("map");
-
 map = L.map('map').setView([51.505,-0.09],13);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -9,6 +7,5 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-alert("LA CON DE TOI");
 
 var marker = L.marker([51.5, -0.09]).addTo(map);
